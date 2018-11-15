d104ec6e1c10688817fbea3384e0fbd93a594c61
Nesuma <71duha1bif@hft-stuttgart.de>
2018-11-15 12:12:03 +0100

    commit updated

--- src/server/settings.py
+++ src/server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import psycopg2
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,6 +126,10 @@ STATIC_URL = '/static/'
 
 #Django postgres connection
 import dj_database_url
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
